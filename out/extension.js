95e9919d44cd98b79c356e4dc6231346f9742f8e
KobeRiddle.dev <koberiddle@gmail.com>
2024-05-22 10:49:50 -0600

    Added repo to package.json. Packaged using vsce.

---
+++ out/extension.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+function activate(context) {
+}
+exports.activate = activate;
+// This method is called when your extension is deactivated
+function deactivate() { }
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
